37d016519e2ec713a952d09cd369d86171263b53
Ashish Pondit <ashish.pondit@dsinnovators.com>
2024-03-14 10:58:38 +0600

    Add capability check

--- db/access.php
+++ db/access.php
@@ -15,27 +15,38 @@
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
- * Plugin strings are defined here.
+ * Plugin version and other meta-data are defined here.
  *
  * @package     local_greetings
- * @category    string
  * @copyright   2024 Ashish Pondit <ashish.pondit@dsinnovators.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Greetings';
-$string['greetinguser'] = 'Greetings, user.';
-$string['greetingloggedinuser'] = 'Greetings, {$a}';
-
-$string['greetinguserau'] = 'Hello, {$a}.';
-$string['greetinguseres'] = 'Hola, {$a}.';
-$string['greetinguserfj'] = 'Bula, {$a}.';
-$string['greetingusernz'] = 'Kia, Ora, {$a}.';
-$string['greetinguserbd'] = 'ওহে, {$a}.';
-$string['yourmessage'] = 'Enter your thoughts here ...';
-$string['postedby'] = 'Posted by {$a}';
-$string['greetings:viewmessages'] = 'View messages on the greetings wall';
-$string['greetings:postmessages'] = 'Post a new message on the Greeting wall';
-$string['greetings:deleteanymessages'] = 'Delete a message from greeting wall';
+$capabilities = [
+    'local/greetings:postmessages' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'user' => CAP_ALLOW,
+        ],
+    ],
+    'local/greetings:viewmessages' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'user' => CAP_ALLOW,
+        ],
+    ],
+    'local/greetings:deleteanymessages' => [
+        'riskbitmask' => RISK_DATALOSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+                'user' => CAP_ALLOW,
+        ],
+    ],
+];
--- index.php
+++ index.php
@@ -36,9 +36,25 @@ if (isguestuser()) {
     throw new moodle_exception('noguest');
 }
 
+$allowpost = has_capability('local/greetings:postmessages', $context);
+
+$deleteanypost = has_capability('local/greetings:deleteanymessages', $context);
+
+$action = optional_param('action', '', PARAM_TEXT);
+
+if ($action == 'del') {
+    require_capability('local/greetings:deleteanymessages', $context);
+
+    $id = required_param('id', PARAM_TEXT);
+
+    $DB->delete_records('local_greetings_messages', ['id' => $id]);
+}
+
+
 $messageform = new \local_greetings\form\message_form();
 
 if ($data = $messageform->get_data()) {
+    require_capability('local/greetings:postmessages', $context);
     $message = required_param('message', PARAM_TEXT);
 
     if (!empty($message)) {
@@ -57,7 +73,10 @@ if (isloggedin()) {
 } else {
     echo get_string('greetinguser', 'local_greetings');
 }
-$messageform->display();
+
+if ($allowpost) {
+    $messageform->display();
+}
 
 $userfields = \core_user\fields::for_name()->with_identity($context);
 $userfieldssql = $userfields->get_sql('u');
@@ -68,22 +87,37 @@ $sql = "SELECT m.id, m.message, m.timecreated, m.userid
      LEFT JOIN {user} u ON u.id = m.userid
       ORDER BY timecreated DESC";
 
-$messages = $DB->get_records_sql($sql);
+$allowview = has_capability('local/greetings:viewmessages', $context);
 
-echo $OUTPUT->box_start('card_columns');
+if ($allowview) {
+    $messages = $DB->get_records_sql($sql);
 
-foreach ($messages as $m) {
-    echo html_writer::start_tag('div', array('class' => 'card'));
-    echo html_writer::start_tag('div', array('class' => 'card-body'));
-    echo html_writer::tag('p', format_text($m->message, FORMAT_PLAIN), array('class' => 'card-text'));
-    echo html_writer::tag('p', get_string('postedby', 'local_greetings', $m->firstname), array('class' => 'card-text'));
-    echo html_writer::start_tag('p', array('class' => 'card-text'));
-    echo html_writer::tag('small', userdate($m->timecreated), array('class' => 'text-muted'));
-    echo html_writer::end_tag('p');
-    echo html_writer::end_tag('div');
-    echo html_writer::end_tag('div');
+    echo $OUTPUT->box_start('card_columns');
+
+    foreach ($messages as $m) {
+        echo html_writer::start_tag('div', ['class' => 'card']);
+        echo html_writer::start_tag('div', ['class' => 'card-body']);
+        echo html_writer::tag('p', format_text($m->message, FORMAT_PLAIN), ['class' => 'card-text']);
+        echo html_writer::tag('p', get_string('postedby', 'local_greetings', $m->firstname), ['class' => 'card-text']);
+        echo html_writer::start_tag('p', ['class' => 'card-text']);
+        echo html_writer::tag('small', userdate($m->timecreated), ['class' => 'text-muted']);
+        echo html_writer::end_tag('p');
+        if ($deleteanypost) {
+            echo html_writer::start_tag('p', ['class' => 'card-footer text-center']);
+            echo html_writer::link(
+                    new moodle_url(
+                            '/local/greetings/index.php',
+                            ['action' => 'del', 'id' => $m->id]
+                    ),
+                    $OUTPUT->pix_icon('t/delete', '') . get_string('delete')
+            );
+            echo html_writer::end_tag('p');
+        }
+        echo html_writer::end_tag('div');
+        echo html_writer::end_tag('div');
+    }
+
+    echo $OUTPUT->box_end();
 }
-
-echo $OUTPUT->box_end();
 
 echo $OUTPUT->footer();
